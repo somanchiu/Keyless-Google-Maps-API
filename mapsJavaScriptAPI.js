22af591670634939419b875aedd8b9b22cfda7b7
somanchiu <somanchiu1996@hotmail.com>
2018-12-21 22:30:03 +0800

    update regex

--- mapsJavaScriptAPI.js
+++ mapsJavaScriptAPI.js
@@ -6,7 +6,7 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var script = document.createElement('script');
-        script.innerHTML = this.responseText.replace(/d.src=b;/, 'if(b.indexOf("common.js")==-1){d.src=b;}else{var formData = new FormData();formData.append("url", b);var xhr = new XMLHttpRequest();xhr.onreadystatechange = function () {    if (this.readyState == 4 && this.status == 200) {var script=document.createElement(\'script\');script.innerHTML=this.responseText.replace(/if\\\(!b\\\){(.*)Failure\\\(\\\)}/,"");document.head.appendChild(script);}};xhr.open("POST", "https://api-cors.7e14.starter-us-west-2.openshiftapps.com/", true);xhr.send(formData);return;}');
+        script.innerHTML = this.responseText.replace(/d.src=b;/, 'if(b.indexOf("common.js")==-1){d.src=b;}else{var formData = new FormData();formData.append("url", b);var xhr = new XMLHttpRequest();xhr.onreadystatechange = function () {    if (this.readyState == 4 && this.status == 200) {var script=document.createElement(\'script\');script.innerHTML=this.responseText.replace(/if\\\(!b\\\){(.*)Failure\\\(\\\)}/s,"");document.head.appendChild(script);}};xhr.open("POST", "https://api-cors.7e14.starter-us-west-2.openshiftapps.com/", true);xhr.send(formData);return;}');
         document.head.appendChild(script);
     }
 };
