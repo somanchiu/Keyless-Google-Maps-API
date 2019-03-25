3199171480dec55e5b6c1fbb06305b7102372fed
somanchiu <somanchiu1996@hotmail.com>
2019-03-25 21:33:09 +0800

    replace the code dynamically

--- mapsJavaScriptAPI.js
+++ mapsJavaScriptAPI.js
@@ -6,7 +6,7 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var script = document.createElement('script');
-        script.innerHTML = this.responseText.replace(/d.src=b;/, 'if(b.indexOf("common.js")==-1){d.src=b;}else{var formData = new FormData();formData.append("url", b);var xhr = new XMLHttpRequest();xhr.onreadystatechange = function () {    if (this.readyState == 4 && this.status == 200) {var script=document.createElement(\'script\');script.innerHTML=this.responseText.replace(/if\\\(!b\\\){(.*)Failure\\\(\\\)}/s,"");document.head.appendChild(script);}};xhr.open("POST", "https://js-cors.herokuapp.com/", true);xhr.send(formData);return;}');
+        script.innerHTML = this.responseText.replace(/d.src=b;/, 'if(b.indexOf("common.js")==-1){d.src=b;}else{var formData = new FormData();formData.append("url", b);var xhr = new XMLHttpRequest();xhr.onreadystatechange = function () {    if (this.readyState == 4 && this.status == 200) {var dynamicVarName = this.responseText.match(/\\\.getStatus\\\(\\\)\\\|\\\|2==(.*?);/)[1];var script=document.createElement(\'script\');script.innerHTML=this.responseText.replace(new RegExp("if\\\\\(!"+dynamicVarName+"\\\\\){(.*)Failure\\\\\(\\\\\)}", "s"),"");document.head.appendChild(script);}};xhr.open("POST", "https://js-cors.herokuapp.com/", true);xhr.send(formData);return;}');
         document.head.appendChild(script);
     }
 };
