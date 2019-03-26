dafd00e0992e62a09bddc06fffe2c953a14f154f
somanchiu <somanchiu1996@hotmail.com>
2019-03-26 20:22:35 +0800

    change the request server

--- mapsJavaScriptAPI.js
+++ mapsJavaScriptAPI.js
@@ -1,14 +1,12 @@
-var formData = new FormData();
 var args = '';
 if (typeof language != 'undefined') args += '&language=' + language;
-formData.append("url", 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDIJ9XX2ZvRKCJcFRrl-lRanEtFUow4piM&callback=initMap' + args);
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var script = document.createElement('script');
-        script.innerHTML = this.responseText.replace(/d.src=b;/, 'if(b.indexOf("common.js")==-1){d.src=b;}else{var formData = new FormData();formData.append("url", b);var xhr = new XMLHttpRequest();xhr.onreadystatechange = function () {    if (this.readyState == 4 && this.status == 200) {var dynamicVarName = this.responseText.match(/\\\.getStatus\\\(\\\)\\\|\\\|2==(.*?);/)[1];var script=document.createElement(\'script\');script.innerHTML=this.responseText.replace(new RegExp("if\\\\\(!"+dynamicVarName+"\\\\\){(.*)Failure\\\\\(\\\\\)}", "s"),"");document.head.appendChild(script);}};xhr.open("POST", "https://js-cors.herokuapp.com/", true);xhr.send(formData);return;}');
+        script.innerHTML = this.responseText.replace(/d.src=b;/, 'if(b.indexOf("common.js")==-1){d.src=b;}else{var xhr=new XMLHttpRequest();xhr.onreadystatechange=function(){if(this.readyState==4&&this.status==200){var dynamicVarName=this.responseText.match(/\\\.getStatus\\\(\\\)\\\|\\\|2==(.*?);/)[1];var script=document.createElement(\'script\');script.innerHTML=this.responseText.replace(new RegExp("if\\\\\(!"+dynamicVarName+"\\\\\){(.*)Failure\\\\\(\\\\\)}","s"),"");document.head.appendChild(script);}};xhr.open("GET","https://cors-anywhere.herokuapp.com/"+b,true);xhr.send();return;}');
         document.head.appendChild(script);
     }
 };
-xhr.open("POST", "https://js-cors.herokuapp.com/", true);
-xhr.send(formData);
+xhr.open("GET", 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/js?key=AIzaSyDIJ9XX2ZvRKCJcFRrl-lRanEtFUow4piM&callback=initMap' + args, true);
+xhr.send();
