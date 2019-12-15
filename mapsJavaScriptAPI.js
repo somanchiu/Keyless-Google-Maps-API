cac5164c385e69be28dd8f9d4ef4923344fe41c8
somanchiu <somanchiu1996@hotmail.com>
2019-12-15 15:26:41 +0800

    For new dynamic var

--- mapsJavaScriptAPI.js
+++ mapsJavaScriptAPI.js
@@ -4,7 +4,10 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var script = document.createElement('script');
-        script.innerHTML = this.responseText.replace(/d.src=b;/, 'if(b.indexOf("common.js")==-1){d.src=b;}else{var xhr=new XMLHttpRequest();xhr.onreadystatechange=function(){if(this.readyState==4&&this.status==200){var dynamicVarName=this.responseText.match(/\\\.getStatus\\\(\\\)\\\|\\\|2==(.*?);/)[1];var script=document.createElement(\'script\');script.innerHTML=this.responseText.replace(new RegExp("if\\\\\(!"+dynamicVarName+"\\\\\){(.*)Failure\\\\\(\\\\\)}","s"),"");document.head.appendChild(script);}};xhr.open("GET","https://cors-anywhere.herokuapp.com/"+b,true);xhr.send();return;}');
+        var temp=this.responseText.match(/;(\w{1}).appendChild\((\w{1})\)}/);
+        var headDynamicVarName=temp[1];
+        var loadJSscript=temp[2];
+        script.innerHTML = this.responseText.replace(headDynamicVarName+'.appendChild('+loadJSscript+')', 'if('+loadJSscript+'.src.indexOf("common.js")==-1){'+headDynamicVarName+'.appendChild('+loadJSscript+')'+';}else{var xhr=new XMLHttpRequest();xhr.onreadystatechange=function(){if(this.readyState==4&&this.status==200){var dynamicVarName=this.responseText.match(/\\\.getStatus\\\(\\\)\\\|\\\|2==(.*?);/)[1];var script=document.createElement(\'script\');script.innerHTML=this.responseText.replace(new RegExp("if\\\\\(!"+dynamicVarName+"\\\\\){(.*)Failure\\\\\(\\\\\)}","s"),"");document.head.appendChild(script);}};xhr.open("GET","https://cors-anywhere.herokuapp.com/"+'+loadJSscript+'.src,true);xhr.send();return;}');
         document.head.appendChild(script);
     }
 };
