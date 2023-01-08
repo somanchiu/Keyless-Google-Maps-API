dfe15209383f1a15cb642515b624ed0b5c3f1d88
somanchiu <somanchiu1996@hotmail.com>
2023-01-08 10:10:16 +0800

    rewrite bypass fn due to google maps api js update

--- mapsJavaScriptAPI.js
+++ mapsJavaScriptAPI.js
@@ -3,20 +3,23 @@ var proxyURL = 'https://api.allorigins.win/raw?url=';
 var args = '';
 if (typeof language != 'undefined') args += '&language=' + language;
 
-var bypass = function (googleAPIcomponent, proxyURL) {
-    if (googleAPIcomponent.src.indexOf("common.js") == -1) {
-        document.head.appendChild(googleAPIcomponent);
+var bypass = function (googleAPIcomponentJS, googleAPIcomponentURL, proxyURL) {
+    if (googleAPIcomponentURL.toString().indexOf("common.js") == -1) {
+        googleAPIcomponentJS.src = googleAPIcomponentURL;
     } else {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                var script = document.createElement('script');
-                script.innerHTML = this.responseText.replace(new RegExp(/;if\(!\w{1}\){.*Failure\(\)}/s), ";");
-                document.head.appendChild(script);
-            }
-        };
-        xhr.open("GET", proxyURL + googleAPIcomponent.src, true);
-        xhr.send();
+        var removeFailureAlert = function(googleAPIcomponentURL, proxyURL) {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var script = document.createElement('script');
+                    script.innerHTML = this.responseText.replace(new RegExp(/;if.*Failure.*\}/), ";");
+                    document.head.appendChild(script);
+                }
+            };
+            xhr.open("GET", proxyURL + googleAPIcomponentURL, true);
+            xhr.send();
+        }
+        googleAPIcomponentJS.innerHTML = '(' + removeFailureAlert.toString() + ')("' + googleAPIcomponentURL.toString() + '","' + proxyURL + '")';
     }
 }
 
@@ -24,9 +27,10 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var script = document.createElement('script');
-        var appendChildToHeadJS = this.responseText.match(/;\w{1}.appendChild\((\w{1})\)/);
-        var googleAPIcomponent = appendChildToHeadJS[1];
-        script.innerHTML = this.responseText.replace(appendChildToHeadJS[0], ';(' + bypass.toString() + ')(' + googleAPIcomponent + ', "' + proxyURL + '")');
+        var appendChildToHeadJS = this.responseText.match(/(\w+)\.src=(_.*?);/);
+        var googleAPIcomponentJS = appendChildToHeadJS[1];
+        var googleAPIcomponentURL = appendChildToHeadJS[2];
+        script.innerHTML = this.responseText.replace(appendChildToHeadJS[0], '(' + bypass.toString() + ')(' + googleAPIcomponentJS + ', ' + googleAPIcomponentURL + ', "' + proxyURL + '");');
         document.head.appendChild(script);
     }
 };
