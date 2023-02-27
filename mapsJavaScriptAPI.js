9d08874b34c8c66813f6af799bb7fa3855288985
somanchiu <somanchiu1996@hotmail.com>
2023-02-27 18:54:10 +0800

    change CROS proxy

--- mapsJavaScriptAPI.js
+++ mapsJavaScriptAPI.js
@@ -1,38 +1,47 @@
-var proxyURL = 'https://api.allorigins.win/raw?url=';
+var CROSproxyURL = 'https://www.whateverorigin.org/get?url=';
 
 var args = '';
 if (typeof language != 'undefined') args += '&language=' + language;
 
-var bypass = function (googleAPIcomponentJS, googleAPIcomponentURL, proxyURL) {
+var bypass = function (googleAPIcomponentJS, googleAPIcomponentURL) {
     if (googleAPIcomponentURL.toString().indexOf("common.js") == -1) {
         googleAPIcomponentJS.src = googleAPIcomponentURL;
     } else {
-        var removeFailureAlert = function(googleAPIcomponentURL, proxyURL) {
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    var script = document.createElement('script');
-                    script.innerHTML = this.responseText.replace(new RegExp(/;if.*Failure.*\}/), ";");
-                    document.head.appendChild(script);
-                }
-            };
-            xhr.open("GET", proxyURL + googleAPIcomponentURL, true);
-            xhr.send();
+        var removeFailureAlert = function(googleAPIcomponentURL) {
+            sendRequestThroughCROSproxy(googleAPIcomponentURL,(responseText)=>{
+                var script = document.createElement('script');
+                script.innerHTML = responseText.replace(new RegExp(/;if.*Failure.*\}/), ";");
+                document.head.appendChild(script);
+            });
         }
-        googleAPIcomponentJS.innerHTML = '(' + removeFailureAlert.toString() + ')("' + googleAPIcomponentURL.toString() + '","' + proxyURL + '")';
+        googleAPIcomponentJS.innerHTML = '(' + removeFailureAlert.toString() + ')("' + googleAPIcomponentURL.toString() + '")';
     }
 }
 
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        var script = document.createElement('script');
-        var appendChildToHeadJS = this.responseText.match(/(\w+)\.src=(_.*?);/);
-        var googleAPIcomponentJS = appendChildToHeadJS[1];
-        var googleAPIcomponentURL = appendChildToHeadJS[2];
-        script.innerHTML = this.responseText.replace(appendChildToHeadJS[0], '(' + bypass.toString() + ')(' + googleAPIcomponentJS + ', ' + googleAPIcomponentURL + ', "' + proxyURL + '");');
-        document.head.appendChild(script);
-    }
-};
-xhr.open("GET", proxyURL + encodeURIComponent('https://maps.googleapis.com/maps/api/js?key=:)&callback=initMap' + args), true);
-xhr.send();
+var createAndExecutePayload = function (googleAPIjs){
+    var script = document.createElement('script');
+    var appendChildToHeadJS = googleAPIjs.match(/(\w+)\.src=(_.*?);/);
+    var googleAPIcomponentJS = appendChildToHeadJS[1];
+    var googleAPIcomponentURL = appendChildToHeadJS[2];
+    script.innerHTML = googleAPIjs.replace(appendChildToHeadJS[0], '(' + bypass.toString() + ')(' + googleAPIcomponentJS + ', ' + googleAPIcomponentURL + ');');
+    document.head.appendChild(script);
+}
+
+sendRequestThroughCROSproxy('https://maps.googleapis.com/maps/api/js?key=:)&callback=initMap' + args, (googleAPIjs)=>{
+    createAndExecutePayload(googleAPIjs);
+});
+
+function sendRequestThroughCROSproxy(url, callback){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if(this.status == 200){
+                if(callback) callback(JSON.parse(this.responseText).contents);
+            }else{
+                sendRequestThroughCROSproxy(url, callback);//retry
+            }
+        }
+    };
+    xhttp.open("GET", CROSproxyURL + encodeURIComponent(url), true);
+    xhttp.send();
+}
